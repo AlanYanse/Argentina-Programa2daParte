b2c20f249e95765c23775d417bc1b8fc38a4e8f2
Alan Yanse <yansealan@gmail.com>
2022-03-08 13:20:31 -0300

    creando nuevo componente en app Angular

--- Modulo3/Angular/myapp/src/app/app.module.ts
+++ Modulo3/Angular/myapp/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { PruebaComponent } from "./prueba-component/prueba-component.component"
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponent
   ],
   imports: [
     BrowserModule
