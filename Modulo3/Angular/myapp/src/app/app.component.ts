019f61b45e326cd7ce8cc9977081e2bdf4afdca1
Alan Yanse <yansealan@gmail.com>
2022-03-15 14:42:20 -0300

    trabajando el routing

--- Modulo3/Angular/myapp/src/app/app.component.ts
+++ Modulo3/Angular/myapp/src/app/app.component.ts
@@ -8,28 +8,5 @@ import { CatService } from './shared/cat.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
-  say: string = '';
-  @Input() toSay: string = "";
-  catSrc: any = "";
-
-  constructor(private catService: CatService) {
-    
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    // Es importante google y consultar la docmumentación
-    // https://angular.io/api/core/OnChanges
-    this.getCat(changes['toSay'].currentValue);
-  }
-
-  async getCat(message: string) {
-    this.catSrc = await this.catService.get(message);
-  }
-
-  onSay(message: string) {
-    this.toSay = message;
-    alert(message);
-    this.getCat(message);
-  }
+  
 }
--- Modulo3/Angular/myapp/src/app/app.module.ts
+++ Modulo3/Angular/myapp/src/app/app.module.ts
@@ -7,16 +7,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PruebaComponent } from "./prueba-component/prueba-component.component"
 import { CatService } from './shared/cat.service';
+import { AppRoutingModule } from './app-routing.module';
+import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
+import { HomeComponent } from './home-component/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PruebaComponent,
+    NotFoundComponentComponent,
+    HomeComponent,
     
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
     
   ],
   providers: [
