63c8d989de83793a557a23829f7153add9afcbb7
Alan Yanse <yansealan@gmail.com>
2022-03-21 07:32:22 -0300

    agregando el componente header

--- Modulo3/Angular/myapp/src/app/app.module.ts
+++ Modulo3/Angular/myapp/src/app/app.module.ts
@@ -3,32 +3,24 @@ import { FormsModule } from '@angular/forms';
 
 
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { PruebaComponent } from "./prueba-component/prueba-component.component"
-import { CatService } from './shared/cat.service';
-import { AppRoutingModule } from './app-routing.module';
-import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
-import { HomeComponent } from './home-component/home.component';
-import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { HeaderComponent } from './componentes/header/header.component'; // Lo importó directamente vsc
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PruebaComponent,
-    NotFoundComponentComponent,
-    HomeComponent,
-    FooterComponentComponent,
+    HeaderComponent // Lo registró automáticamente vsc
     
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    
     
   ],
   providers: [
-    CatService,
+    
   ],
   bootstrap: [AppComponent]
   
