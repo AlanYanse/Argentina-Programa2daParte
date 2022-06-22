f24cb3aefe46a000cb52cbac2e222ce83e388294
Alan Yanse <yansealan@gmail.com>
2022-06-22 13:34:42 -0300

    Master clase hasta el minuto 60

---
+++ Modulo6/Master-Clase-Java/src/ejemplopoo/Auto.java
@@ -0,0 +1,109 @@
+package ejemplopoo;
+
+public class Auto {
+	
+	private int id;
+	private String patente;
+	private int cantPuertas;
+	private String color;
+	private boolean cajaManual;
+	
+	// Constructores
+	
+	public Auto() {
+		
+		
+	}
+	
+	public Auto(int id , String patente, int cantPuertas, String color, boolean cajaManual) {
+		
+		this.id = id;
+		this.patente = patente;
+		this.cantPuertas = cantPuertas;
+		this.color = color;
+		this.cajaManual = cajaManual;
+	}
+	
+	// Getters y Setters
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPatente() {
+		return patente;
+	}
+
+	public void setPatente(String patente) {
+		this.patente = patente;
+	}
+
+	public int getCantPuertas() {
+		return cantPuertas;
+	}
+
+	public void setCantPuertas(int cantPuertas) {
+		this.cantPuertas = cantPuertas;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public boolean isCajaManual() {
+		return cajaManual;
+	}
+
+	public void setCajaManual(boolean cajaManual) {
+		this.cajaManual = cajaManual;
+	}
+
+	
+	
+	
+	// Métodos random
+	
+	public void encender() {
+		
+		System.out.println("Estoy encendiendo");
+	}
+		
+	public void apagar() {
+		
+		System.out.println("Estoy apagando");
+	}
+	
+	public void acelerar(double km) {
+		
+		System.out.println("Estoy acelerando a " + km + "km/h");
+	}
+	
+	public void frenar(double km) {
+		
+		System.out.println("Estoy frenando " + km + "km/h");
+	}
+
+	
+	
+	// toString
+	
+	
+	@Override
+	public String toString() {
+		return "Auto [id=" + id + ", patente=" + patente + ", cantPuertas=" + cantPuertas + ", color=" + color
+				+ ", cajaManual=" + cajaManual + "]";
+	}
+	
+	
+	
+	
+
+}
---
+++ Modulo6/Master-Clase-Java/src/ejemplopoo/EjemploPOO.java
@@ -0,0 +1,44 @@
+package ejemplopoo;
+
+public class EjemploPOO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Auto auto1 = new Auto(1 , "AA111J", 5, "Rojo", true );
+
+		
+		Auto auto2 = new Auto();
+		
+		System.out.println(auto1.toString());
+		System.out.println(auto2.toString());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		 		
+		auto1.encender();
+		auto1.acelerar(60);
+		auto1.frenar(30);
+		auto1.apagar();
+		
+		
+		System.out.println("El id del auto2 es : " + auto2.getId());
+		
+		System.out.println("Vamos a cambiar el id del auto2 mediante el setter");
+		
+		auto2.setId(2);
+		
+		System.out.println("Ahora el id del auto2 es : " + auto2.getId());
+		*/
+		
+
+	}
+
+}
