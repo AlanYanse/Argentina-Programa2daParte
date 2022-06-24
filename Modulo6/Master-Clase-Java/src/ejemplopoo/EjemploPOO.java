bff56747ac9b913a7b0d3e1002f1ac6a07c539ba
Alan Yanse <yansealan@gmail.com>
2022-06-24 15:22:47 -0300

    Master Clase completa

--- Modulo6/Master-Clase-Java/src/ejemplopoo/EjemploPOO.java
+++ Modulo6/Master-Clase-Java/src/ejemplopoo/EjemploPOO.java
@@ -6,13 +6,28 @@ public class EjemploPOO {
 		// TODO Auto-generated method stub
 		
 		Camion camioncito;
-		
-		Auto auto1 = new Auto(6532 , 4 ,"Peugeot" , "206", 1 , "AA111J", 5, "Rojo", true );
-		Auto auto2 = new Auto();
+		Moto motito;
+		Vehiculo vehiculo = new Vehiculo(52445, 4, "Renault", "indefinido");
 		
 		camioncito = new Camion();
 		camioncito.setCantRuedas(4);
 		
+		motito = new Moto(1453, 2 , "Yamaha", "ZR200", 125);
+		
+		Auto auto1 = new Auto(6532 , 4 ,"Peugeot" , "206", 1 , "AA111J", 5, "Rojo", true );
+		Auto auto2 = new Auto();
+		
+		Vehiculo arrayVehiculos[] = new Vehiculo[4];
+		
+		arrayVehiculos[0] = vehiculo;
+		arrayVehiculos[1] = camioncito;
+		arrayVehiculos[2] = motito;
+		arrayVehiculos[3] = auto1;
+		
+		for(Vehiculo v : arrayVehiculos) {
+			
+			System.out.println(v.getMarca() + " " + v.getModelo());
+		}
 		
 		System.out.println(auto1.toString());
 		System.out.println(auto2.toString());
